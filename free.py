631c60bbaa5f294388d854d66a9f25dc3d67353f
Udhv <bakaraniyaudhv@gmail.com>
2022-07-17 12:32:57 +0530

    Added Files

---
+++ free.py
@@ -0,0 +1,19 @@
+
+q1=[
+    [1,"world first computer ?","ENIAC ","Tata Institute of Fundamental Research Automatic Calculator (TIFRAC)","Common Operating Machine ",1],
+    [2,"2.how many generations of computers we have ?","one","ten","five",3],
+    [3,'Who is the father of Computer science ?','stephen hawking','Charles Babbage','abdul kalam',2],
+    [4,'In a computer, most processing takes place in _______?','mouse','.keyboard','CPU',3],
+    [5,'Who is the father of personal computer?','Edward Robert','Charles Babbage','kevin mitnik',1],
+    [6,'IBM stands for _______?','International Business Machines','Interpreted business machines','international barrier machines',1],
+    [7,'.DOS stands for _______?','denial operating system','Disk Operating System','deniled operating system',2],
+    [8,'Refresh Rate of monitor is measured in ?','Hertz','pixels','mm',1],
+    [9,'Where are the CPU and memory located?','mouse','motherboard','processor',2],
+    [10,'Full form of MAN?','motrologic area network','Metropolitan Area Network','Monition area network',2],
+    [11,'In which year, the Microsoft company was founded?','1975','1972','1965',1],
+    [12,'What are mouse and joystick?','input devices','prcessing devices','.output devices',1],
+    [13,'What kind of memory is both static and non-volatile?','RAM','VRAM','ROM',3],
+    [14,'Where does most data go first with in a computer memory hierarchy?','RAM','ROM','VRAM',1],
+    [15,'When did arch rivals IBM and Apple Computers Inc. decide to join hands?','1991','1975','1968',1]
+]
+    
