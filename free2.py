e89eb828177b35ec4d9f462e11b7b7e673e10e0d
Udhv <109453276+Udhv@users.noreply.github.com>
2022-07-17 15:50:23 +0530

    Add files via upload

---
+++ free2.py
@@ -0,0 +1,67 @@
+
+from flask import Flask, render_template, request
+import icmodule
+
+app = Flask(__name__)
+
+
+
+class Question:
+    q_id=-1
+    question=""
+    option1=""
+    option2=""
+    option3=""
+    option4=""
+    correctOption=-1
+
+    def __init__(self,q_id,question,option1,option2,option3,option4,correctOption):
+        self.q_id= q_id
+        self.question=question
+        self.option1=option1
+        self.option2=option2
+        self.option3=option3
+        self.option4=option4
+        self.correctOption=correctOption
+
+    def get_correct_option(self):
+        
+        if self.correctOption==1:
+            return self.option1
+        elif self.correctOption==2:
+            return self.option2
+        elif self.correctOption==3:
+            return self.option3
+        elif self.correctOption==4:
+            return self.option4
+        
+
+question_list=[]
+for x in range(15):
+    q=(icmodule.q1[x])
+    q1=Question(q[0],q[1],q[2],q[3],q[4],q[5],q[6])
+    question_list.append(q1)
+    
+@app.route("/")
+def quiz():
+    return render_template("quiz.html",question_list=question_list)
+
+@app.route("/submitquiz",methods=['POST','GET'])
+
+def submit():
+    
+    p="Your Score is"
+    correct_count=0
+    for question in question_list:
+        question_id=str(question.q_id)
+        selected_option=request.form[question_id]
+        correct_option=question.get_correct_option()
+        if selected_option==correct_option:
+            correct_count=correct_count+1
+    correct_count=str(correct_count) 
+    return ("<br>"+"<br>"+"<br>"+"<br>"+"<br>"+'<h1 style="text-align:center;">'+p+correct_count+'</h1>')
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ icmodule.py
@@ -0,0 +1,24 @@
+
+q1=[
+    [1,"The members of the Rajya Sabha are elected by elected","A.members of the Legislative Assembly","B.members of Lok Sabha","C.members of Rajya Sabha","D.members of Supreme court","A"],
+    [2,"Total number of High Courts in India is?","A.24","B.25", "C.26","D.27","A"],
+    [3,"Who was the first Indian Women President of the United Nations General Assembly.","A.Vijay Lakshmi Pandit","B.Lata Devi","C.Indira Gandhi","D.Sarojini Devi","A"],
+    [4,'"Which one of the following article deals with the appointment of the Prime Minister and other ministers?',"A.Article 76","B.Article 74","C.Article 75 ","D.Article 72","C"],
+    [5,'The Prime Minister is appointed by which one of the following?',"A.Attorney general of India","B.President","C.Vice-president", "D.Chief justice of India","B"],
+    [6,'The Supreme Court of India at present contains the following number of Judges?'," A.25 judges"," B.31 judges", "C.20 judges","D.30 judges","B"],
+    [7,"Who among the following appoints the Chief Justice and other Judges of the Supreme Court?","A.Prime minister","B.Vice-president","C.Home minister","D.President","D"],
+    [8,"Which of the following are the powers of the Supreme Court?","A.Original and Appellate Jurisdiction","B.Appointment of ad-hoc judges","C.Judicial review","D.All The Above","D"],
+    [9,"Mr. T.S. Thakur is the ...................Chief Justice of India.","A.41st","B.42nd","C.43rd","D.44th","C"],
+    [10,"The executive power of the Union is vested in which one of the below?","A.Prime Minister","B.Home Minister","C.Vice President","D.President","D"],
+    [11,"When was the first Parliamentary Forum on Water Conservation and Management constituted?","A.1950","B.2005","C.1970","D.1985","B"],
+    [12,"When was the first Parliamentary Forum on Youth constituted?","A.2010","B.2008","C.2006","D.1985","C"],
+    [13,"The independence of Judiciary”  in Indian constitution is taken from.","A.Britain","B.USA","C.South Africa","C.Austalia","A"],
+    [14,"Which of the following article of the Indian Constitution deals with the constitution of the Parliament of India?","A.Article 73","B.Article 78","C.Article 79","D.Article 72","C"],
+    [15,"How many number of members are nominated by the President to the Rajya sabha?","A.20","B.18","C.12","D.15","C"],
+    [16,"which court decesion is final and can't be challenged?","A.Local court","B.District court","C.High court","D.Supreme court"],
+    [17,"Which is known as the upper house ?","A.Lok sabha","B.jan sabha","c.Rajya sabha","d.Local sabha"],
+    [18,"Lok sabha has how many seats ? ","A.245","B.345","C.445","D.545"],
+    [19,"Rajya sabha total number of seats ? ","A.100","B.150","C.200","D.250"],
+    [20,"Government has total how many powers ? ","A.10","B.15","C.5","D.7"],
+]
+
