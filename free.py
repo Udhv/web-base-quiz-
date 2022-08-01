8b20eb2e3d3aa95588c9cc077cd33217f56443ea
Udhv <109453276+Udhv@users.noreply.github.com>
2022-08-01 22:55:06 +0530

    Update free.py

--- free.py
+++ free.py
@@ -1,12 +1,12 @@
 
 q1=[
     [1,"world first computer ?","ENIAC ","Tata Institute of Fundamental Research Automatic Calculator (TIFRAC)","Common Operating Machine ",1],
-    [2,"2.how many generations of computers we have ?","one","ten","five",3],
+    [2,"how many generations of computers we have ?","one","ten","five",3],
     [3,'Who is the father of Computer science ?','stephen hawking','Charles Babbage','abdul kalam',2],
     [4,'In a computer, most processing takes place in _______?','mouse','.keyboard','CPU',3],
     [5,'Who is the father of personal computer?','Edward Robert','Charles Babbage','kevin mitnik',1],
     [6,'IBM stands for _______?','International Business Machines','Interpreted business machines','international barrier machines',1],
-    [7,'.DOS stands for _______?','denial operating system','Disk Operating System','deniled operating system',2],
+    [7,'DOS stands for _______?','denial operating system','Disk Operating System','deniled operating system',2],
     [8,'Refresh Rate of monitor is measured in ?','Hertz','pixels','mm',1],
     [9,'Where are the CPU and memory located?','mouse','motherboard','processor',2],
     [10,'Full form of MAN?','motrologic area network','Metropolitan Area Network','Monition area network',2],
